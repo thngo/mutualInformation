9f5546cfa85bed4fa70d47b66c34ac349fa1521f
thngo <tra.huongngo@gmail.com>
2022-03-25 21:50:43 -0500

    Correct variables with test[0] & add imports
    Correct variable "termList" by calculating log before multiplying the probability of each states (bins)  of the sample. Code not tested.

--- calc_mult_MI.py
+++ calc_mult_MI.py
@@ -1,36 +1,61 @@
-#from TN's derivation of multivariate MI
+import numpy as np
+import pandas as pd
+import statistics as st
+import re
+import csv
+import scanpy as sc 
+import scanpy.external as sce
+import phate
+import matplotlib
+from matplotlib import pyplot as plt
+import warnings
+from scipy import stats
+from scipy.stats import binom
+from scipy.stats import multinomial
+import seaborn as sns
+from scipy.stats import hypergeom
+import warnings
+warnings.filterwarnings('ignore')
+import pickle
+from random import sample
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import scipy
+import scipy.io as scio
+import itertools
+
+# from TN's derivation of multivariate MI
 
 def calc_mult_MI(M,bins):
     totLen = len(M)
     ## Getting probabilities ################
     c = list()
-    #getting the histogram distributions 
-    
-    # individual probabilities
-    
-        
-    for i in totLen:
+    # getting the histogram distributions 
+       
+    for i in np.arange(totLen):
         # Get combination of probabilities 
         tmp = list(range(0, totLen)) # range 0 to totLen because python start indexing at 0
-        combList = list(itertools.combinations(tmp, i)) # get number of iterations
+        combList = list(itertools.combinations(tmp, i+1)) # get number of iterations
         # Calculate probabilities, for each combinations
-        ind_prob = list()
+        ind_prob = list() # individual probabilities at each bin
         termList = list()
-        for j in len(combList):
+        for j in np.arange(len(combList)):
             # Create subset
             M_sub = list() # initialize subset
-            for iSub in test[k]:
+            for iSub in combList[j]:
                 M_sub.append(M[iSub])
+            # Frequency observed in each bin of this M_sub sample
             ind_prob.append(probNorm(M_sub,bins))
         # Calculate each log term
-            # i.e. (-1)^0*log(P(X1)*P(X2)*P(X3)*...*P(Xn)) for individual probabilities
-            # Sign is (+) if number of variables is ODD when we calculate the probabilities
-            # i.e. (-1)^1*log(P(X1,X2)*P(X1,X3)*P(X2,X3)) for joint probabilities
-            # Sign is (-) if number of variables is EVEN when we calculate the probabilities
-        termList.append(pow(-1,j)*np.log(ny.nanprod(ind_prob)))
+            # i.e. (-1)^1*log(P(X1)*P(X2)*P(X3)*...*P(Xn)) for individual probabilities
+            # Sign is (-) if number of variables is ODD when we calculate the probabilities
+            # i.e. (-1)^2*log(P(X1,X2)*P(X1,X3)*P(X2,X3)) for joint probabilities
+            # Sign is (+) if number of variables is EVEN when we calculate the probabilities
+        termList.append(pow(-1,j+1)*np.log(ind_prob))
+        # termList.append(pow(-1,j+1)*np.log(np.nanprod(ind_prob))) # power to (j+1) because python start indexing at 0
         
     # Calculate MI
-    MI = prob_TN(M,bins) * np.nanprod(termList)
+    MI = probNorm(M,bins) * np.nanprod(termList)
     return MI
 
 def probNorm(M,bins):
